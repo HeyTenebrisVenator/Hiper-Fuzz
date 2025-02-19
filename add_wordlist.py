5f6dba51280ec69afdf7aa5d478e684c7fdd4baf
Arthur Witt <168667004+HeyTenebrisVenator@users.noreply.github.com>
2025-02-19 14:23:35 -0300

    Update add_wordlist.py

--- add_wordlist.py
+++ add_wordlist.py
@@ -19,7 +19,7 @@ os.system(f'sudo cp -r {local} /usr/share/hiper_fuzz/wordlists')
 print(f'Wordlist {local} added successfully')
 print(f'Analyzing hiper_fuzz.py')
 hiper_fuzz = open('hiper_fuzz.py', 'r').read()
-value = 0
+value = 1
 
 while True:
     if f'def Wordlist{value}' in hiper_fuzz:
@@ -29,29 +29,38 @@ while True:
         break
 
 
-
 new_wordlist_command = f"""
 def Wordlist{value}():
-    wordlist = 'wordlists/{local.split('/')[-1].replace(' ', '')}
+    wordlist = 'wordlists/{local.split('/')[-1].replace(' ', '')}'
     lbl4['text'] = f'Wordlist: {{wordlist}}'
-wordlist4 = Button(window, text='Common API endpoints', command=Wordlist{value})
+wordlist4 = Button(window, text='{name}', command=Wordlist{value})
 wordlist4.grid(column=1, row={value + 2})
 window.mainloop()"""
+
+print(new_wordlist_command)
+
 print('Command created')
 print('Regenerating hiper_fuzz.py')
 
 final_script = ""
 hiper_fuzz = open('hiper_fuzz.py', 'r').readlines()
 for line in hiper_fuzz:
+    line = line.replace('\n', '')
     if 'window.mainloop()' in line:
-        line.replace('window.mainloop()', new_wordlist_command)
-    final_script += line
+        line = line.replace('window.mainloop()', new_wordlist_command)
+    final_script += line + '\n'
 
 print('Script generated successfully')
 print('Recreating hiper_fuzz.py')
 
+
+print(final_script)
+
 try:
     open('hiper_fuzz.py', 'w').write(final_script)
+    print('DONE')
 except:
     print('Could not write to hiper_fuzz.py')
     exit()
+
+os.system('sudo python3 install.py')
