1c161845001ea7767c2d8901d4b7b4d6d5f62bb8
Arthur Witt <168667004+HeyTenebrisVenator@users.noreply.github.com>
2025-02-23 19:05:55 -0300

    Update install.py

--- install.py
+++ install.py
@@ -7,7 +7,7 @@ try:
 except FileExistsError:
     print('File already exists')
 os.system('sudo cp -r wordlists /usr/share/hiper_fuzz/wordlists')
-os.system('sudo cp hiper_fuzz.py /usr/local/bin/hiper_fuzz')
+os.system('sudo cp hiper_fuzz.py /usr/local/bin/hiper_fuzz; sudo chmod +x /usr/local/bin/hiper_fuzz')
 os.system('sudo apt-get install python3-tk')
 os.system('sudo apt-get install ffuf')
 os.system('sudo apt install gnome-terminal')
